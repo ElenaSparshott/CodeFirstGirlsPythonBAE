f98cc5b1a0339ccced37f7502db04f25a21c6f69
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-09-24 09:33:40 +0100

    First commit

---
+++ cat_food.py
@@ -0,0 +1,8 @@
+cats = 4
+cans = 3
+days = 7
+
+num = cats * cans * days
+
+output = "We need {} cans!".format(num)
+print(output)
---
+++ hello.py
@@ -0,0 +1,6 @@
+print('Hello, World! everybody dance now')
+print('Hello, World! everybody dance now')
+print("Hi there")
+print(456 + 400)
+print('no')
+
