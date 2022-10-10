c762474e7974ab9c0457e490c50f4ad8995bff28
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-10-10 21:59:24 +0100

    CFG homework session 3

---
+++ CFGHomeworkSessionThree.py
@@ -0,0 +1,27 @@
+# rain = input("Is it raining? y/n ")
+# if rain == "y":
+#     print("Take an umbrella")
+# if rain == "n":
+#     print("Don't take the umbrella")
+#
+#
+# my_money = float(input('How much money do you have? '))
+# boat_cost = 20 + 5
+# if my_money > boat_cost:
+#     print('You can afford the boat hire')
+# else:
+#     print('You cannot afford the board hire')
+
+
+year = float(input('Please enter a year between 1800 and 1950: '))
+
+if 1800 <= year <= 1809:
+    print("Nineteenth Century, naughties")
+elif 1810 <= year <= 1819:
+    print ("Nineteenth Century, tens")
+elif 1820 <= year <= 1829:
+    print ("Nineteenth Century, twenty")
+
+
+
+
