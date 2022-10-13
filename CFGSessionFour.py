e603ace4444c2854cc7b5698d7705c6db5fc80a3
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-10-13 20:24:37 +0100

    session 4

---
+++ CFGSessionFour.py
@@ -0,0 +1,9 @@
+import random
+
+names = ["Ellie", "Izzy", "Poppy", "Edward", "David"]
+myChoice = random.choice(names)
+
+verbs = ["runs", "walks", "jogs"]
+myVerbs = random.choice(verbs)
+
+print(f'{myChoice} {myVerbs}')
