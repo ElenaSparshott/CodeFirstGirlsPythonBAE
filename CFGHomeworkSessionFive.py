7bf3c36c76ba6f89f019cdce0d51605fafe860b8
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-10-26 13:24:26 +0100

    question 3

--- CFGHomeworkSessionFive.py
+++ CFGHomeworkSessionFive.py
@@ -1,3 +1,5 @@
+# Question one.
+#
 # What is PIP?
 # PIP is a package manager for Python packages, or modules if you like.
 #
@@ -6,5 +8,34 @@
 # What is a Package?
 # A package contains all the files you need for a module.
 #
-# Modules are Python code libraries you can include in your project.
+# Modules are Python code libraries you can include in your project. You can use
+# for example complex maths library that someone else has written.
+# Then use that in your own code.
+
+
+# Question two.
+
+# poem = "I like Python and I am not very good at poems"
+#
+# with open("poem.txt,", "r") as poem_file:
+#     poem_file.write(poem)
+
+# The open() contains "r" which is read.  If we want to write to a file
+# We need to use "w"
+
+
+# Question three
+
+import requests
+
+pokemonIds = [1, 2, 3, 4, 5, 6,]
+
+for x in pokemonIds:
+    url = "https://pokeapi.co/api/v2/pokemon/{}/".format(x)
+    response = requests.get(url)
+    pokemon = response.json()
+
+    print(pokemon['name'])
+    print(pokemon['moves'])
+
 
